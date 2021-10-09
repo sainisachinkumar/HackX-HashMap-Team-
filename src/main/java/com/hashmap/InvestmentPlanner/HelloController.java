779d38e1a89c4bf46aaabecfebbc40865bedd4d2
Kailash Desiti <kailashdesiti5@gmail.com>
2021-10-09 16:15:45 +0530

    project setup

---
+++ src/main/java/com/hashmap/InvestmentPlanner/HelloController.java
@@ -0,0 +1,16 @@
+package com.hashmap.InvestmentPlanner;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@CrossOrigin(origins = "*")
+	@GetMapping("/index")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
---
+++ src/test/java/com/hashmap/InvestmentPlanner/InvestmentPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashmap.InvestmentPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
